275c069f254c1478dd1bb6961273b610b32362ef
Federico <romerof14@gmail.com>
2022-08-23 10:11:52 -0300

    FEAT/Deploy: Correcciones para poder hacer el deploy.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ dotenv.config();
 const config = {
   dev: process.env.NODE_ENV !== 'production',
   port: process.env.API_PORT,
-  host: process.env.API_host,
+  host: process.env.API_HOST,
   cors: process.env.CORS,
 };
 
